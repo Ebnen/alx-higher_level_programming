da940d03f98ea31193dceb4d51148f5862aabcb7
Ebnen <ebenzy1996@gmail.com>
2024-04-08 09:12:30 -0700

    sdjhs

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Creation of Module"""
+
+
+class Square:
+
+    """define a sqaure"""
+
+    def __init__(self, size):
+        """contructor
+        Args:
+           size of square
+        """
+
+        self.__size = size
