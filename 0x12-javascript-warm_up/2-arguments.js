311a5c4eb85362ff1e22b61741e11f26df14366d
Ebnen <Ebenzy1996@gmail.com>
2024-06-19 17:30:41 +0100

    ddf

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const bold = process.argv.slice(2);
 if (bold.length === 0) {
-    console.log('No argument');
-}else if (bold.length === 1) {
-    console.log('Argument found');
-}else 
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (bold.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
