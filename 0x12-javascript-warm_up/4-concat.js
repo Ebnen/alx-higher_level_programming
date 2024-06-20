011c094859bae952950e2f4b0022ae4c3b9f90d6
Ebnen <Ebenzy1996@gmail.com>
2024-06-20 01:48:17 +0100

    ddf

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const bold = process.argv.slice(2);
-if (bold.length !== 2) {
-  console.log('cant help you');
-} else {
-  const [arg1, arg2] = bold;
-  console.log(`${arg1} is ${arg2}`);
-}
+const [arg1 = 'undefined', arg2 = 'undefined'] = bold;
+console.log(`${arg1} is ${arg2}`);
