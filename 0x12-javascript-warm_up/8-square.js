ee0d1255aff54766d11f1ccc91fc6be8550bca3d
Ebnen <Ebenzy1996@gmail.com>
2024-06-20 03:21:19 +0100

    ddf

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const bold = process.argv.slice(2);
+const [arg1] = bold;
+const x = Number(arg1);
+if (Number.isInteger(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('#'.repeat(x));
+  }
+} else {
+  console.log('Missing number');
+}
